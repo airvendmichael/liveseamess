f2944ed393c6aae7fc67ea462dc9905893b77c02
Airvend <airvend@airvend.ng>
2021-05-25 14:47:16 +0100

    removed function

--- function/db.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-global $mysqli;
-$mysqli   = mysqli_connect("127.0.0.1", "callphone", "Click!@#", "airvend");
- 
-?>
--- function/testdb.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli   = mysqli_connect("127.0.0.1", "callphone", "Click!@#", "airvend");
-
-if($mysqli){
-echo "connected";}else{echo "die";}
